f946c8957b5a44c15bac1b44fab83ab8a4397adf
Amazingmoondeer <deuswambiji07@gmail.com>
2025-07-02 11:04:58 +0300

    Update and rename index.html to package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "music-downloader-player",
+  "version": "1.0.0",
+  "main": "main.js",
+  "scripts": {
+    "start": "electron .",
+    "backend": "node backend.js"
+  },
+  "dependencies": {
+    "electron": "^25.0.0",
+    "express": "^4.18.0",
+    "yt-dlp-exec": "^1.0.2"
+  }
+}
